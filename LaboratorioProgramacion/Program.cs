5bb662f85e547b9e779aca0d84e6dee25da13204
Martino Tomas Recio <paulorecio81@gmail.com>
2025-03-28 13:43:29 -0300

    Agregar archivos de proyecto.

---
+++ LaboratorioProgramacion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioProgramacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Lindo viernes para estar con Visual Studio 2022!");
+
+        }
+    }
+}
